fb64905018f94b8eb5806d71244038f9fee6ca49
fabricio <fabriciossza@gmail.com>
2023-03-17 17:38:45 -0300

    feat(script): structuring api request with async/await function

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-const newMovieButton = document.querySelector('.shuffle_button')
-
-const movieId = 550
-const movieInfo = 'overview'
-
-async function requestApi(movieId, movieInfo) {
-    response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=db6f487c3c45474eb9298e5f522dfbf5`)
-    result = await response.json()
-
-    return result[movieInfo]
+// async function to get the json api info
+async function requestMovie(movieId) {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=db6f487c3c45474eb9298e5f522dfbf5`)
+    const result = await response.json()
+    return result
 }
 
-console.log(requestApi(550, 'overview'))
+function getRandom() {
+    return Math.floor(Math.random() * 1000) + 1;
+}
+
+const newMovieButton = document.querySelector('.shuffle_button');
+
+newMovieButton.onclick = () => {
+    let randomNumber = getRandom()
+    newTitle(randomNumber)
+    newSynopsis(randomNumber)
+}
 
 
+const movieTitle = document.querySelector('#movie_title')
+const movieSynopsis = document.querySelector('#movie_synopsis')
 
-// .then(response => response.json())
-// .then(result => {
-//     console.log(result.original_title)
-//     console.log(result[movieInfo])
-//     console.log(result.popularity)
-// })
+
+async function newTitle(movieId) {
+    let title =  await requestMovie(movieId)
+    movieTitle.innerHTML = title.original_title
+}
+
+async function newSynopsis(movieId) {
+    let synopsis =  await requestMovie(movieId)
+    movieSynopsis.innerHTML = synopsis.overview
+}
+
+
