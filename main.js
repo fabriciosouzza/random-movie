d598de40e391cc469525d333e77ae7faceaa77ee
fabricio <fabriciossza@gmail.com>
2023-03-20 11:04:26 -0300

    feat(script): add IMDb Rating and movie poster request

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// async function to get the json api info
 async function requestMovie(movieId) {
     const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=db6f487c3c45474eb9298e5f522dfbf5`)
     const result = await response.json()
@@ -6,30 +5,47 @@ async function requestMovie(movieId) {
 }
 
 function getRandom() {
-    return Math.floor(Math.random() * 1000) + 1;
+    return Math.floor(Math.random() * 5000) + 1;
 }
 
 const newMovieButton = document.querySelector('.shuffle_button');
 
 newMovieButton.onclick = () => {
     let randomNumber = getRandom()
+    newPoster(randomNumber)
+    
     newTitle(randomNumber)
     newSynopsis(randomNumber)
+    newRating(randomNumber)
 }
 
 
 const movieTitle = document.querySelector('#movie_title')
 const movieSynopsis = document.querySelector('#movie_synopsis')
+const movieRating = document.querySelector('#movie_rating')
+const moviePoster = document.querySelector('.movie_poster')
 
 
 async function newTitle(movieId) {
     let title =  await requestMovie(movieId)
-    movieTitle.innerHTML = title.original_title
+    movieTitle.textContent = title.original_title
 }
 
 async function newSynopsis(movieId) {
     let synopsis =  await requestMovie(movieId)
-    movieSynopsis.innerHTML = synopsis.overview
+    movieSynopsis.textContent = synopsis.overview
+}
+
+async function newRating(movieId) {
+    let rating =  await requestMovie(movieId)
+    let roundedRating = rating.vote_average.toFixed(2)
+    movieRating.textContent = `IMDb Rating: ${roundedRating}`
+}
+
+async function newPoster(movieId) {
+    let poster =  await requestMovie(movieId)
+    let posterUrl = `https://image.tmdb.org/t/p/w500/${poster.poster_path}`
+    moviePoster.src = posterUrl
 }
 
 
